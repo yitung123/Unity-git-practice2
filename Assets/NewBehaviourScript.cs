93602df4d62a3f40f9b85e12132c4b54d87aaa00
rStar <starlove1247@gmail.com>
2025-02-27 15:00:08 +0800

    第一個程式碼

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("awake");
+    }
+
+    private void Start()
+    {
+        Debug.Log($"Start");
+    }
+}
