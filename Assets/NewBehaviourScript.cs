c0edafd279f642ec315cf688dad05a9b38287c36
rStar <starlove1247@gmail.com>
2025-03-13 15:03:31 +0800

    A碰到B，開啟C物件Active，C預設是關閉Active

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,40 +5,23 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("遊戲執行 Start");
-    }
+    [SerializeField]
+    private GameObject box;
 
-    private void Awake()
-    {
-        Debug.Log($"Awake");
-    }
-
-    private void OnEnable()
-    {
-        Debug.Log($"OnEnable");
-    }
-
-    private void OnDisable()
-    {
-        Debug.Log($"OnDisable");
-    }
-
-    private void OnDestroy()
-    {
-        Debug.Log($"OnDestroy");
-    }
-
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log($"碰到地板");
+        // other.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
+        // GameObject hand = GameObject.Find("Hand"); // 尋找名為Hand遊戲物件，並儲存為區域變數
         Debug.Log($"進入偵測區域");
+        // GameObject box = GameObject.Find("寶箱");
+        box.SetActive(true);
+        // GameObject.Find("寶箱").SetActive(true);
+        // gameObject.SetActive(false);
     }
 
     private void OnTriggerExit2D(Collider2D other)
